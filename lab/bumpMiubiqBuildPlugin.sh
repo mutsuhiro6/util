fcf18c2f76a86ee6c72afc8c71e5f71341b3882f
Mutsuhiro Iwamoto <iwamoto@miubiq.cs.titech.ac.jp>
2020-07-02 03:15:16 +0900

    update

--- lab/bumpMiubiqBuildPlugin.sh
+++ lab/bumpMiubiqBuildPlugin.sh
@@ -9,10 +9,12 @@ echo $to
 repoName=$(basename $(git remote -v |  awk '{print $2}') |  sed 's/\.git//g')
 currentBranch=`git rev-parse --abbrev-ref HEAD`
 bumpBranch=feature/bump_miubiq_build_plugin_from_$1_to_$2
+stash=1
 
 if [ -n "$(git status --porcelain)" ]; then 
     # Stash uncommit changes and untracked files
     git stash -u
+    stash=0
 fi
 
 git checkout develop
@@ -20,10 +22,10 @@ git pull
 
 git checkout -b $bumpBranch \
 && sed -i "" -e "s/classpath \"gradle\.plugin\.org\.miubiq:miubiq-build-plugin:$from\"/classpath \"gradle\.plugin\.org\.miubiq:miubiq-build-plugin:$to\"/g" build.gradle  \
-&& git commit -am "bump miubiq build plugin version from $from to $to" \
+&& git commit -am "bump miubiq build plugin version from $1 to $2" \
 && git push -u origin $bumpBranch \
-&& open https://bitbucket.org/icslab/$repoName/pull-requests/new?dest=icslab/$repoName::develop&source=icslab/$repoName::$bumpBranch#diff
+&& open "https://bitbucket.org/icslab/$repoName/pull-requests/new?dest=icslab/$repoName::develop&source=icslab/$repoName::$bumpBranch#diff"
 
-git checkout $currentBranch && git stash pop
+git checkout $currentBranch && if [ $stash = 0]; then git stash pop; fi
 
 echo -e "I bumped miubiq-build-plugin version from $1 to $2.\nI appreciate it if you make prompt approval.\nThank you for your cooperation.
--- lab/bumpMlToolkit.sh
+++ lab/bumpMlToolkit.sh
@@ -9,21 +9,23 @@ echo $to
 repoName=$(basename $(git remote -v |  awk '{print $2}') |  sed 's/\.git//g')
 currentBranch=`git rev-parse --abbrev-ref HEAD`
 bumpBranch=feature/bump_mltoolkit_from_$1_to_$2
+stash=1
 
 if [ -n "$(git status --porcelain)" ]; then 
     # Stash uncommit changes and untracked files
     git stash -u
+    stash=0
 fi
 
 git checkout develop
 git pull
 
 git checkout -b $bumpBranch \
-&& sed -i "" -e "s/compile(group: \'org\.miubiq\.commons\', name: \'mltoolkit\', version: \'$from\')/s/compile(group: \'org\.miubiq\.commons\', name: \'mltoolkit\', version: \'$to\')/g" build.gradle \
-&& git commit -am "bump mltoolkit version from $from to $to" \
+&& sed -i "" -e "s/compile(group: \'org\.miubiq\.commons\', name: \'mltoolkit\', version: \'$from\')/compile(group: \'org\.miubiq\.commons\', name: \'mltoolkit\', version: \'$to\')/g" build.gradle \
+&& git commit -am "bump mltoolkit version from $1 to $2" \
 && git push -u origin $bumpBranch \
-&& open https://bitbucket.org/icslab/$repoName/pull-requests/new?dest=icslab/$repoName::develop&source=icslab/$repoName::$bumpBranch#diff
+&& open "https://bitbucket.org/icslab/$repoName/pull-requests/new?dest=icslab/$repoName::develop&source=icslab/$repoName::$bumpBranch#diff"
 
-git checkout $currentBranch && git stash pop
+git checkout $currentBranch && if [ $stash = 0 ]; then git stash pop; fi
 
-echo -e "I bumped mltoolkit version from $1 to $2.\nI appreciate it if you make prompt approval.\nThank you for your cooperation.
+echo -e "I bumped mltoolkit version from $1 to $2.\nI appreciate it if you make prompt approval.\nThank you for your cooperation."
