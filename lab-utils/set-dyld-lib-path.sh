72871f8b072fa42f9e7846e5844ce39f22b2466d
Mutsuhiro Iwamoto <iwamoto@miubiq.cs.titech.ac.jp>
2020-11-16 14:59:40 +0900

    update dyld setter

--- lab-utils/set-dyld-lib-path.sh
+++ lab-utils/set-dyld-lib-path.sh
@@ -12,3 +12,7 @@ function set-dyld-v95 {
 	export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$MCR_ROOT/v95/runtime/maci64:$MCR_ROOT/v95/sys/os/maci64:$MCR_ROOT/v95/bin/maci64:$MCR_ROOT/v95/extern/bin/maci64
 }
 
+function set-dyld-v98 {
+        export MCR_ROOT=/Applications/MATLAB/MATLAB_Runtime
+        export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$MCR_ROOT/v98/runtime/maci64:$MCR_ROOT/v98/sys/os/maci64:$MCR_ROOT/v98/bin/maci64:$MCR_ROOT/v98/extern/bin/maci64
+}
