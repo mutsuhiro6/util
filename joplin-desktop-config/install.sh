22af0513535d30b056af1037eb88a543bc601b93
Mutsuhiro Iwamoto <mutsuhiro6@icloud.com>
2021-01-27 10:29:48 +0900

    add joplin configs

---
+++ joplin-desktop-config/install.sh
@@ -0,0 +1,5 @@
+JOPLIN_CFG_DIR="$HOME/.config/joplin-desktop"
+
+ln userstyle.css $JOPLIN_CFG_DIR/userstyle.css
+ln userchrome.css $JOPLIN_CFG_DIR/userchrome.css
+ln templates/diary-template.md $JOPLIN_CFG_DIR/templates/diary-template.md
