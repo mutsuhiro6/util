fd23e988fb75740d1451c051ba67773e82679ddd
miwamo <21per.nm@gmail.com>
2021-03-23 15:02:30 +0900

    Delete url_shortener.sh

--- url_shortener.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-curl -H "Host: api-ssl.bitly.com
-Authorization: Bearer 14ec0be35d1fdace8753bca119eb1fabc61be07c
-Content-Type: application/json" --http1.1 -X POST -d '{"group_guid": "Bj61epXWImP", "domain": "bit.ly", "long_url": "'"$1"'"}' https://api-ssl.bitly.com/v4/shorten | jq -r '.link'
